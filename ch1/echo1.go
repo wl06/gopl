c1060d0260480553b939cdec017dfc8ab815e713
wxl <1402949402@qq.com>
2019-09-03 12:47:18 +0800

    初始化

---
+++ ch1/echo1.go
@@ -0,0 +1,16 @@
+// 短变量声明,for循环
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
+}
---
+++ ch1/echo2.go
@@ -0,0 +1,16 @@
+// range
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	s, sep := "", ""
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+}
